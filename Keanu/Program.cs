22e7f0f67a32d31be488e982a9d35528ec7317be
Nathalia Vieira <nathaliavieira.ms@gmail.com>
2022-09-05 17:44:00 -0300

    first commit

---
+++ Keanu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Keanu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine((numero * numero) - ((numero * numero) / 2) + " casas brancas e " + ((numero * numero) / 2) + " casas pretas");
+        }
+    }
+}
